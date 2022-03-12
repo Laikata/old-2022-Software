bf7123864152e2fea2109d34b0648fd6c15cdb93
Daniel Kauss <manuel.eckol2001@gmail.com>
2022-03-12 12:52:49 +0100

    "Finished" gps library

---
+++ lib/gps/src/gps.cpp
@@ -0,0 +1,24 @@
+#include "gps.h"
+#include <Arduino.h>
+#include <TinyGPSPlus.h>
+#include <vector.h>
+#include <SoftwareSerial.h>
+
+static const int RXPin = D5, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPSPlus object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+vec3_t *getPosition(){
+    vec3_t *pos = vec3_init(gps.location.lng(), gps.location.lat(), gps.altitude.meters());
+    return pos;
+}
+
+void gpsInit(){
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+}
