a71d98b629d2323ac2f793f310751e3a80850d20
Josep Mengual <josepmengual@protonmail.com>
2022-03-13 23:05:13 +0100

    receive working

---
+++ src/comms_test/recv.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial ss(10,11);
+
+int count = 0;
+
+void setup() {
+  Serial.begin(115200);
+  ss.begin(9600);
+  ss.write("AT+P8");
+  Serial.println("Hello");
+}
+
+void loop() {
+  if (ss.available()) {
+    Serial.write(ss.read());
+  }
+}
