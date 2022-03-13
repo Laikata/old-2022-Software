a0369a4860303a5f5b3b163f803fb0826c0e5eea
Josep Mengual <josepmengual@protonmail.com>
2022-03-14 00:36:11 +0100

    q no m

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ void setup() {
   comms_init();
 }
 
-unsigned char test_message[14]= "short message";
+unsigned char test_message[13]= {'s', 'h', 'o', 'r', 't', ' ', 'm', 'e', 's', 's', 'a', 'g', 'e'};
 
 void loop() {
   send(test_message, sizeof(test_message));
