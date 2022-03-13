704b25a136f4be93018c96f3aee8aa19477b1639
Josep Mengual <josepmengual@protonmail.com>
2022-03-13 20:31:34 +0100

    libcomms: Make crc_table a constant in header

--- lib/comms/src/comms.c
+++ lib/comms/src/comms.c
@@ -57,36 +57,15 @@ int recv(char *data[]) {
 	return 0;
 }
 
-uint32_t crc32(const uint8_t data[], size_t data_length, const uint32_t crctable[256]) {
+uint32_t crc32(const uint8_t data[], size_t data_length) {
 	uint32_t crc32 = 0xFFFFFFFFu;
 	
 	for (size_t i = 0; i < data_length; i++) {
 		const uint32_t lookupIndex = (crc32 ^ data[i]) & 0xff;
-		crc32 = (crc32 >> 8) ^ crctable[lookupIndex];  // CRCTable is an array of 256 32-bit constants
+		crc32 = (crc32 >> 8) ^ crc_table[lookupIndex];  // CRCTable is an array of 256 32-bit constants
 	}
 	
 	// Finalize the CRC-32 value by inverting all the bits
 	crc32 ^= 0xFFFFFFFFu;
 	return crc32;
-}
-
-const uint32_t * make_crc_table()
-{
-	uint32_t crc_table[256];
-	
-	uint32_t c;
-	int16_t n, k;
-
-	for (n = 0; n < 256; n++) {
-	c = (uint32_t) n;
-	for (k = 0; k < 8; k++) {
-		if (c & 1)
-		c = 0xedb88320L ^ (c >> 1);
-		else
-		c = c >> 1;
-	}
-	crc_table[n] = c;
-	}
-	
-	return crc_table;
 }
