20e343ad72452edcf26b159c4cf663097b26a1ee
Josep Mengual <josepmengual@protonmail.com>
2022-03-14 01:18:27 +0100

    pra

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,12 @@
 #include <SoftwareSerial.h>
 #include <comms.h>
 
+unsigned char test_message[13]= {'s', 'h', 'o', 'r', 't', ' ', 'm', 'e', 's', 's', 'a', 'g', 'e'};
+
 void setup() {
   comms_init();
 }
 
-unsigned char test_message[13]= {'s', 'h', 'o', 'r', 't', ' ', 'm', 'e', 's', 's', 'a', 'g', 'e'};
 
 void loop() {
   send(test_message, sizeof(test_message));
