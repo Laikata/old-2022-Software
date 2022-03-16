8e5d4e3952ccfc51d2ce925feaf6f5ad4d030b85
DanielKauss <DanielKauss@users.noreply.github.com>
2022-03-16 19:10:48 +0100

    fix nav

--- lib/nav/src/nav.h
+++ lib/nav/src/nav.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector.h>
 
-float nav_angle(vec3_t pos, vec3_t target, float dir);
 float angleBetweenPoints(vec3_t p1, vec3_t p2);
+float nav_angle(vec3_t pos, vec3_t target, float dir);
+
 
