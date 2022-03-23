3d698c11b4fccafd8b9bf897a3a89cf8090464d2
Arnau478 <arnauxabia@gmail.com>
2022-03-23 19:56:54 +0100

    Another 1000 errors fixed...

--- lib/sensors/src/sensors.cpp
+++ lib/sensors/src/sensors.cpp
@@ -11,55 +11,47 @@ DHT22 dht22;
 Adafruit_BMP085 bmp;
 MPU9250 mpu;
 
-
-sensors::sensors(int AM2302Pin)
-{
-  bmp.begin();
-  DHT22 dht22 = DHT22(AM2302Pin);
-
-}
-
-float sensors::bmpAltitude(){
+float sensors_bmpAltitude(){
     return(bmp.readAltitude());
 }
 
-float sensors::bmpPressure(){
+float sensors_bmpPressure(){
     return(bmp.readPressure());
 }
 
-float sensors::bmpSealevelPressure(){
+float sensors_bmpSealevelPressure(){
     return(bmp.readSealevelPressure());
 }
 
-float sensors::amTemperature(){
+float sensors_amTemperature(){
     return(dht22.readTemperature());
 }
 
-float sensors::amHumidity(){
+float sensors_amHumidity(){
     return(dht22.readHumidity());
 }
 
 //Leave the cansat on a flat plane
-void sensors::mpuCalibrateAccel(){
+void sensors_mpuCalibrateAccel(){
     mpu.calibrateAccelGyro();
 }
 
 //Wave cansat in a figure eight until done
-void sensors::mpuCalibrateMag(){
+void sensors_mpuCalibrateMag(){
     mpu.calibrateMag();
 }
 
-float sensors::mpuAccelerometer(){
+float sensors_mpuAccelerometer(){
     float _accelerometer[3] = {mpu.getAccX(), mpu.getAccY(), mpu.getAccZ()};
     return (_accelerometer[3]);
 }
 
-float sensors::mpuGyro(){
+float sensors_mpuGyro(){
     float _gyro[3] = {mpu.getGyroX(), mpu.getGyroY(), mpu.getGyroZ()};
     return (_gyro[3]);
 }
 
-float sensors::mpuMagnetometer(){
+float sensors_mpuMagnetometer(){
     float _Magnetometer[3] = {mpu.getMagX(), mpu.getMagY(), mpu.getMagZ()};
     return (_Magnetometer[3]);
 }
