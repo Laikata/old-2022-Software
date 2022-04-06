52f3c30c6dfd45a4d22af37fbb849834632382db
Josep Mengual <salmon@truita.es>
2022-04-06 19:58:08 +0200

    Return vector.cpp

---
+++ lib/vector/src/vector.cpp
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <vector.h>
+
+vec3_t *vec3_init(float x, float y, float z){
+    vec3_t *vec = (vec3_t *)malloc(sizeof(vec3_t));
+    vec->x = x;
+    vec->y = y;
+    vec->z = z;
+    return vec;
+}
+
+void vec3_free(vec3_t *vec){
+    free(vec);
+}
+
+
+
+vec3_t *vec3_sub(vec3_t *a, vec3_t *b){
+    vec3_t *res = (vec3_t *)malloc(sizeof(vec3_t));
+    res->x = a->x - b->x;
+    res->y = a->y - b->y;
+    res->z = a->z - b->z;
+    return res;
+}
+
+float vec3_deg(vec3_t *vec){
+    if(vec->x == 0){
+        printf("ERROR: vector x component is 0\n");
+        exit(1);
+        return (float)0;
+    }
+    return atan(vec->y/vec->x);
+}
