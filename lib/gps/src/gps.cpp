18d62e0dcf34e46872d6c0f068491f3aa4f9cc75
Josep Mengual <salmon@truita.es>
2022-04-27 21:04:57 +0200

    Don't use SoftwareSerial in gps_init

--- lib/gps/src/gps.cpp
+++ lib/gps/src/gps.cpp
@@ -2,17 +2,12 @@
 #include <Arduino.h>
 #include <TinyGPSPlus.h>
 #include <vector.h>
-#include <SoftwareSerial.h>
 #include <stdint.h>
 
-static const int RXPin = D5, TXPin = 3;
 static const uint32_t GPSBaud = 9600;
 
 // The TinyGPSPlus object
 TinyGPSPlus gps;
-
-// The serial connection to the GPS device
-SoftwareSerial ss(RXPin, TXPin);
 
 vec3_t *gps_position(){
     vec3_t *pos = vec3_init(gps.location.lng(), gps.location.lat(), gps.altitude.meters());
@@ -24,5 +19,5 @@ int gps_satellites(){
 }
 
 void gps_init(){
-    ss.begin(GPSBaud);
+    Serial.begin(GPSBaud);
 }
