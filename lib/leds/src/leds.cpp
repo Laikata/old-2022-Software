6221e1948d19dbe56a120beb53a12c49a17b359a
Tiliy6 <>
2022-05-14 18:27:20 +0200

    Implemented updateLeds()

--- lib/leds/src/leds.cpp
+++ lib/leds/src/leds.cpp
@@ -2,10 +2,36 @@
 
 #include <FastLED.h>
 
+CRGB leds[8];
+
 void initLeds(){
-    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, 8);
+    FastLED.addLeds<WS2812B, D5, RGB>(leds, 8);
 }
 
 void updateLeds(){
+    float valor = analogRead(A0);
+    valor = valor*4.7f/1023;
 
+    static unsigned long tNextLed = millis();
+    static unsigned long tNextOff;
+    
+    if (millis() > tNextLed) {
+        tNextLed = millis() + 2000;
+        if (valor >= 3.8) {
+            leds[0] = CRGB::Green;
+        }
+        else if (valor <= 3.6) {
+            leds[0] = CRGB::Red;
+        }
+        else {
+            leds[0] = CRGB::Yellow;
+        }
+        FastLED.show();
+        tNextOff = millis() + 25;
+    }
+
+    if (millis() > tNextOff) {
+        leds[0] = CRGB::Black;
+        FastLED.show();
+    }
 }
