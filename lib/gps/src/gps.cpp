7e6a55d0f4a33f436b7f2bad9871bdb3173d8f85
DiverBOT-Educativa <39483235+DiverBOT-Educativa@users.noreply.github.com>
2022-05-17 14:06:02 +0200

    Update gps.cpp

--- lib/gps/src/gps.cpp
+++ lib/gps/src/gps.cpp
@@ -12,8 +12,15 @@ TinyGPSPlus gps;
 
 SoftwareSerial gpsSerial (D6, D7);
 
+void displayInfo();
+
 vec3_t gps_position(){
+    while(gpsSerial.available()>0){
+        if (gps.encode(gpsSerial.read() ) )
+            displayInfo();
+    }
     vec3_t pos = {gps.location.lng(), gps.location.lat(), gps.altitude.meters()};
+    Serial.printf("GPS_DIRECTO: (%g, %g, %g)\n", gps.location.lng(), gps.location.lat(), gps.altitude.meters());
     return pos;
 }
 
@@ -23,4 +30,55 @@ int gps_satellites(){
 
 void gps_init(){
     gpsSerial.begin(GPSBaud);
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
 }
