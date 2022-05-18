b38f8e1fe7309f396304c091dda5a432d6c9c5c9
Josep Mengual <salmon@truita.es>
2022-05-18 20:37:38 +0200

    Now calculate_direction uses float degrees, instead of radians

--- lib/dir/dir.cpp
+++ lib/dir/dir.cpp
@@ -1,12 +1,15 @@
 #include "dir.h"
-#include <Arduino.h>
 #include <vector.h>
+
+#ifndef PI
+#define PI 3.14159265
+#endif
 
 int calculate_direction(float *dir, float des_lat, float des_lon, float des_hei, float can_lat, float can_lon, float can_hei){
     vec3_t can = {can_lat, can_lon, can_hei};
     vec3_t des = {des_lat, des_lon, des_hei};
     vec3_t diff = vec3_sub(&des, &can);
-    *dir = vec3_deg(&diff) + PI/2;
+    *dir = vec3_deg(&diff)/(2*PI)*360 + 180;
     // TODO: Check if NaN
     return 0;
 }
